f801d238195ef6f796668c4855c58b7950e2eef4
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-02-07 11:45:10 +0530

    add data to server-POST request

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiUrl = "https://tripy-express-student-neog.replit.app/destinations";
+
+const destinationInputForm = document.querySelector('#destinationInputForm');
+
+destinationInputForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const destinationNameInput = document.querySelector('#destinationNameInput');
+    const destinationLocationInput = document.querySelector('#destinationLocationInput');
+    const destinationDescriptionInput = document.querySelector('#destinationDescriptionInput');
+    const destinationRatingInput = document.querySelector('#destinationRatingInput');
+
+    const newDestinationObj = {
+        name: destinationNameInput.value,
+        location: destinationLocationInput.value,
+        description: destinationDescriptionInput.value,
+        rating: destinationRatingInput.value
+    }
+
+    fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(newDestinationObj),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data) {
+                console.log(data);
+            }
+        })
+
+
+})
